4169379134498d27b14e001a14c45da97dcc1958
Denis Balogh <denbalogh@gmail.com>
2025-04-01 09:00:31 +0200

    setup typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from "typescript-eslint";
+
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  eslintPluginPrettierRecommended,
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
 ]);
